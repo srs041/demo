aa44b96e6186a8d0f60c61deb767235a235228f2
Seetal <gudufb1@gmail.com>
2025-10-10 12:32:35 +0530

    Initial commit

---
+++ src/main/java/com/example/demo/rest/FunRestController.java
@@ -0,0 +1,20 @@
+package com.example.demo.rest;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class FunRestController {
+    //expose a / endpoint that returns hello world
+    @GetMapping("/")
+    public String hello(){
+        return  "Hello World!!!!!!";
+    }
+
+    //exdpose a new endpoing for workout
+    @GetMapping("/workout")
+    public String workout(){
+        return "Run 5k steps !!!";
+    }
+
+}
